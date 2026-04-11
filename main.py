6c5af89e22d151fd964baca13c1266a92e3fbdb3
purdue github account <bejanthk@purdue.edu>
2026-04-11 17:38:56 -0400

    intial_commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are an expert in answering questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is the question to answer: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = prompt|model
+
+while True:
+    question = input("Ask your question (q to quit):  ")
+    print('\n\n')
+    if question == "q":
+        break
+
+reviews = retriever.invoke(question)
+result = chain.invoke({"reviews":reviews, "question":"What is the best pizza place in town?"})
+
+print(result)
+
+
+# from langchain_ollama.llms import OllamaLLM
+
+# model = OllamaLLM(model="llama3.2")
+# response = model.invoke("Why is the sky blue?")
+# print(response)
